45fa9a41d504d6d50902a8d0ff7b44027e4b16e3
Wang Weizhou <wangw222@teach.cs.toronto.edu>
2022-10-30 23:16:36 -0400

    Finished Lab3 BackTracking

---
+++ lab3/battle_validate/battle_validate.py
@@ -0,0 +1,37 @@
+import copy
+import random
+import time
+import queue
+import sys
+import os
+
+# Validation script for CSC384 A3 : battle.py
+# Change input_easy1.txt, output_easy1.txt and solution_easy1.txt to run
+#    other test inputs.
+
+if __name__ == '__main__':
+  if len(sys.argv) != 3:
+    print('Wrong input/output parameters...')
+    exit(1)
+  __, output_file, solution_file = sys.argv
+  
+  # Invoke the shell command to test the checkers solver
+  print("Testing: " + output_file)
+  # os.system("python3 ../battle.py "+ input_file + " " + output_file)
+
+  output_read = open(output_file, "r")
+  solution_read = open(solution_file, "r")
+
+  output_lines = output_read.readlines()
+  solution_lines = solution_read.readlines()
+  passed = True
+
+  for index in range(1, len(output_lines)):
+    if output_lines[index].strip() != solution_lines[index].strip():
+      print(f"Line {index + 1}: "
+                             f"Expected <{output_lines[index].strip()}> "
+                             f"Encountered <{solution_lines[index].strip()}>\n")
+      passed = False
+      break
+
+  print("Battleship output matches solution file.\n")
---
+++ lab3/test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+time python3 battle.py ./battle_validate/input_easy1.txt output_easy1.txt
+python3 ./battle_validate/battle_validate.py output_easy1.txt ./battle_validate/solution_easy1.txt
+
+time python3 battle.py ./battle_validate/input_easy2.txt output_easy2.txt
+python3 ./battle_validate/battle_validate.py output_easy2.txt ./battle_validate/solution_easy2.txt
+
+time python3 battle.py ./battle_validate/input_medium1.txt output_medium1.txt
+python3 ./battle_validate/battle_validate.py output_medium1.txt ./battle_validate/solution_medium1.txt
+
+time  battle.py ./battle_validate/input_medium2.txt output_medium2.txt
+python3 ./battle_validate/battle_validate.py output_medium2.txt ./battle_validate/solution_medium2.txt
+
+time python3 battle.py ./battle_validate/input_hard1.txt output_hard1.txt
+python3 ./battle_validate/battle_validate.py output_hard1.txt ./battle_validate/solution_hard1.txt
+
+time python3 battle.py ./battle_validate/input_hard2.txt output_hard2.txt
+python3 ./battle_validate/battle_validate.py output_hard2.txt ./battle_validate/solution_hard2.txt
