5b83b36a187fe17386f5133d3543eb8feb2b177b
Wang Weizhou <wangw222@teach.cs.toronto.edu>
2022-10-31 14:10:04 -0400

    Updated test file

--- lab3/test.sh
+++ lab3/test.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+rm *out.txt
 for i in $(seq 0 15)
 do
     echo "=====================Test $i====================="
     time python3 battle.py ./battle_validate/$i.txt ${i}out.txt
+    retVal=$?
+    if [ $retVal -ne 0 ]; then
+        exit $retVal
+    fi
     python3 ./battle_validate/battle_validate.py ${i}out.txt ./battle_validate/${i}ref.txt
 done
